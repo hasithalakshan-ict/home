cf669267dc099ca64c938d0294ddac71db5c0401
Lakshan H M H <157282954+hasithalakshan-ict@users.noreply.github.com>
2025-07-03 02:23:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const elements = document.querySelectorAll('.slide-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+elements.forEach(el => observer.observe(el));
+
+
+
